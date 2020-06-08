eec9218258b4f123332a351da9fd9a16a27ea5a3
Michael Orange <michael.orange09@gmail.com>
2020-06-08 14:43:51 +0400

    improve functions

--- display_pca.py
+++ display_pca.py
@@ -5,6 +5,23 @@ import seaborn as sns
 from scipy.cluster.hierarchy import dendrogram
 
 
+def display_corr_matrix(corr):
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=np.bool))
+
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(8, 6))
+    # Generate a custom diverging colormap
+    cmap = sns.color_palette("GnBu_d")
+    # Draw the heatmap with the mask and correct aspect ratio
+    sns.heatmap(corr, annot=True, vmin=-1, vmax=1, mask=mask,
+                cmap=cmap, square=True, center=0, linewidths=.5, cbar_kws={"shrink": .7})
+    # Rotate x labels
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
+
+    plt.title('Correlation Matrix', weight='bold')
+    plt.show()
+
 
 def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None):
     for d1, d2 in axis_ranks:
@@ -17,7 +34,7 @@ def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0,
             if lims is not None:
                 xmin, xmax, ymin, ymax = lims
             elif pcs.shape[1] < 30:
-                xmin, xmax, ymin, ymax = -1, 1, -1, 1
+                xmin, xmax, ymin, ymax = -1*1.05, 1*1.05, -1*1.05, 1*1.05
             else:
                 xmin, xmax, ymin, ymax = min(pcs[d1, :]), max(pcs[d1, :]), min(pcs[d2, :]), max(pcs[d2, :])
                 plt.title("- Zoom - Correlation Circle (F{} et F{})".format(d1 + 1, d2 + 1))
@@ -61,31 +78,33 @@ def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0,
 
 def display_scree_plot(pca):
     scree = pca.explained_variance_ratio_ * 100
-    p = len(scree)
+    n_comp = len(scree)
 
-    bs = 1 / np.arange(p, 0, -1)
+    bs = 1 / np.arange(n_comp, 0, -1)
     bs = np.cumsum(bs)
     bs = bs[::-1]
 
-    plt.bar(np.arange(len(scree))+1, scree)
-    plt.plot(np.arange(len(scree))+1, scree.cumsum(),c="black",marker='o')
+    plt.bar(np.arange(n_comp)+1, scree)
+    plt.plot(np.arange(n_comp)+1, scree.cumsum(),c="black",marker='o')
     plt.xlabel("Factor Number")
     plt.ylabel("Eigenvalue")
     plt.title("Scree plot - Explained variance vs # of factors")
+    plt.xticks(np.arange(n_comp)+1)
     plt.show()
 
 def display_broken_sticks_plot(pca, n):
-    p = len(pca.explained_variance_)
+    n_comp = len(pca.explained_variance_)
 
-    bs = 1 / np.arange(p, 0, -1)
+    bs = 1 / np.arange(n_comp, 0, -1)
     bs = np.cumsum(bs)
     bs = bs[::-1]
 
     eigval = (n - 1) / n * pca.explained_variance_
 
-    plt.bar(np.arange(p) + 1, eigval)
-    plt.plot(np.arange(p) + 1, bs, c='red', marker='o', label='treshold')
+    plt.bar(np.arange(n_comp) + 1, eigval)
+    plt.plot(np.arange(n_comp) + 1, bs, c='red', marker='o', label='treshold')
 
+    plt.xticks(np.arange(n_comp)+1)
     plt.xlabel("Factor Number")
     plt.ylabel("Eigenval")
     plt.title("Broken Stick Model")
@@ -137,29 +156,42 @@ def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, 
                 ax2.set_title("- Zoom - Projection (on F{} and F{})".format(d1 + 1, d2 + 1))
 
 
-def display_factorial_planes\
+def display_factorial_planes \
                 (X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, illustrative_var=None, lims=None):
     for d1, d2 in axis_ranks:
         if d2 < n_comp:
 
-            fig = plt.figure(figsize=(15, 14))
+            sns.set_style("darkgrid")
+            plt.figure(figsize=(12, 12))
+            x_axis = X_projected[:, d1]
+            y_axis = X_projected[:, d2]
 
             # display points
             if illustrative_var is None:
-                plt.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha)
+                sns.scatterplot(x_axis, y_axis, alpha=alpha)
             else:
-                illustrative_var = np.array(illustrative_var)
-                for value in np.unique(illustrative_var):
-                    selected = np.where(illustrative_var == value)
-                    plt.plot(X_projected[selected, d1], X_projected[selected, d2], alpha=alpha, label=value, s=10)
-                plt.legend()
+                sns.scatterplot(x_axis, y_axis, hue=illustrative_var, alpha=alpha, palette='Set1')
 
-            # affichage des labels des points
+                # display centroid only if there is no labels to display
+                if labels is None:
+                    centroids_all = []
+                    for cluster_name in sorted(illustrative_var.unique()):
+                        index_proj = illustrative_var.loc[illustrative_var == cluster_name].index
+                        x_centroid, y_centroid = (np.mean(x_axis[index_proj]), np.mean(y_axis[index_proj]))
+                        centroids_all.append([x_centroid, y_centroid])
+
+                    plt.scatter([row[0] for row in centroids_all], [row[1] for row in centroids_all], s=85, alpha=0.5, marker='o', c='black', label='Centroids')
+
+                    for i, (x, y) in enumerate(centroids_all):
+                        plt.text(x, y + 0.05, sorted(illustrative_var.unique())[i], fontsize='10', weight='bold',
+                                 ha='center', va='bottom')
+
+            # display labels of the points
             if labels is not None:
                 for i, (x, y) in enumerate(X_projected[:, [d1, d2]]):
                     plt.text(x, y + 0.05, labels[i], fontsize='8', ha='center', va='bottom')
 
-            # détermination des limites du graphique
+            # graph limits
             boundary = np.max(np.abs(X_projected[:, [d1, d2]])) * 1.1
             plt.xlim([-boundary, boundary])
             plt.ylim([-boundary, boundary])
@@ -173,14 +205,15 @@ def display_factorial_planes\
                 plt.xlim(xmin, xmax)
                 plt.ylim(ymin, ymax)
 
-            # affichage des lignes horizontales et verticales
+            # display middle lines
             plt.plot([-100, 100], [0, 0], color='grey', ls='--')
             plt.plot([0, 0], [-100, 100], color='grey', ls='--')
 
-            # nom des axes, avec le pourcentage d'inertie expliqué
+            # axes names with value
             plt.xlabel('F{} ({}%)'.format(d1 + 1, round(100 * pca.explained_variance_ratio_[d1], 1)))
             plt.ylabel('F{} ({}%)'.format(d2 + 1, round(100 * pca.explained_variance_ratio_[d2], 1)))
 
+            plt.legend()
             plt.title("Projection (on F{} and F{})".format(d1 + 1, d2 + 1))
 
             plt.show(block=False)
